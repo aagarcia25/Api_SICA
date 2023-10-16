69e943d78a57d92221bb375dde6995b13dc99027
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-10-16 10:54:48 -0600

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -38,13 +38,13 @@ return [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'port' => env('MAIL_PORT', 25),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
-            'SMTPAuth' => false,
+            'SMTPAuth' => true,
         ],
 
         'ses' => [
