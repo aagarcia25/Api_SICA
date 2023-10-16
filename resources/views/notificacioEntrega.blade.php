b84184ad6aa05e586c85e1b1013dfc56e51b6ba7
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-10-16 17:12:03 -0600

    ajustes

--- resources/views/notificacioEntrega.blade.php
+++ resources/views/notificacioEntrega.blade.php
@@ -16,7 +16,14 @@ Notificación de visita agendada
 </ol>
 
 
-<img src="{{ $rutaTemporalqr }}" alt="Código QR">
+   <!-- Verifica si la variable está definida antes de usarla -->
+@if (isset($rutaTemporalqr))
+    <img src="{{ $rutaTemporalqr }}" alt="Código QR">
+@else
+    <!-- Puedes mostrar un mensaje o un marcador de posición en caso de que la variable no esté definida -->
+    <p>La imagen QR no está disponible</p>
+@endif
+
 
     <br /><br /><br />
     Este correo es generado automaticamente  *No Responder*
