a0d160076d70da0c86b8614258eeb73e3d366dd8
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2024-03-01 13:28:58 -0600

    ajustes

--- resources/views/notificacioEntrega.blade.php
+++ resources/views/notificacioEntrega.blade.php
@@ -1,26 +1,30 @@
-
 <h1>
-Notificación de visita agendada
+    Notificación de visita agendada
 </h1>
-<p><h3> Visitante: {{$data->visitante}}</h3></p>
+<p>
+<h3> Visitante: {{ $data->visitante }}</h3>
+</p>
 <br />
-<p><h3><b>{{$data->edificio}}</h3></p>
- <ol>
-  <li>Fecha: {{$data->FechaVisita}}</li>
-  <li>Duración:  {{$data->Duracion}}</li>
-  <li>Dirección: {{$data->Direccion}}</li>
-  <li>Persona a visitar: {{$data->receptor}}</li>
-  <li>{{$data->entidadreceptor }}</li>
-  <li>{{ $data->pisoreceptorrr }} </li>
+<p>
+<h3><b>{{ $data->edificio }}</h3>
+</p>
+<ol>
+    <li>Fecha: {{ $data->FechaVisita }}</li>
+    <li>Duración: {{ $data->Duracion }}</li>
+    <li>Dirección: {{ $data->Direccion }}</li>
+    <li>Persona a visitar: {{ $data->receptor }}</li>
+    <li>Dependencia:{{ $data->entidadreceptor }}</li>
+    <li>{{ $data->pisoreceptorrr }} </li>
+    <li>Observaciones:{{ $data->observaciones }}</li>
 </ol>
 
 
-    <div align="center" class="visible-print text-center">
-      {!! QrCode::size(100)->generate($data->id); !!}
+<div align="center" class="visible-print text-center">
+    {!! QrCode::size(100)->generate($data->id) !!}
     <p>Muestra el código al acudir a tu visita para identificarte fácilmente.</p>
-    </div>
+</div>
 
 
-    <br /><br /><br />
-   <div align="center"> Este correo es generado automaticamente  *No Responder*</div>
-    <br />
+<br /><br /><br />
+<div align="center"> Este correo es generado automaticamente *No Responder*</div>
+<br />
