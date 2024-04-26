21401b688a79d70ec4d0b29b9a02f09f8326792f
EdithLizcano <126712189+EdithLizcano@users.noreply.github.com>
2024-04-25 18:07:04 -0600

    entidad receptor en informe

--- app/Http/Controllers/InfoVisitasController.php
+++ app/Http/Controllers/InfoVisitasController.php
@@ -29,9 +29,9 @@ class InfoVisitasController extends Controller
             SELECT 
             DATE(vi.FechaVisita) AS FechaVisita,
             en.Nombre,
-            COUNT(vi.idEntidad) AS VisitasPorDia
+            COUNT(vi.IdEntidadReceptor) AS VisitasPorDia
         FROM SICA.Visita vi
-        INNER JOIN TiCentral.Entidades en ON vi.idEntidad = en.Id
+        INNER JOIN TiCentral.Entidades en ON vi.IdEntidadReceptor = en.Id
         WHERE vi.deleted = 0 
         
         GROUP BY DATE(vi.FechaVisita), vi.idEntidad, en.Nombre;
